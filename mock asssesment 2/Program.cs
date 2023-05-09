54dcda64bbc5e8596dca50d3769cd6fe482ffbab
reidmedlock <benmedlock19@gmail.com>
2023-05-08 21:31:31 -0400

    Add project files.

---
+++ mock asssesment 2/Program.cs
@@ -0,0 +1,38 @@
+﻿return -1;
+ static int AddStarWarsCharacters(string[] characters)
+{
+    for (int i = 0; i < characters.Length; i++)
+    {
+        if (characters[i] == "Yoda")
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+   
+static int DeathStarCombat(string[] characters)
+{
+    Dictionary<string, int> combat = new Dictionary<string, int>();
+    {
+        {"900","Mace Windu" },
+        {"800" "obi wan"},
+        { "1000", "Yoda"},
+
+
+
+
+    };
+  
+ 
+
+
+
+
+
+}
+
+string ConvertPlanets(string input);
+{
+    string[] planets = new string[8];
+}
